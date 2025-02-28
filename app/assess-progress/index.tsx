abd66906eba73a379824676e21cc264730d7d01d
Bebe Jane <bjornthief@gmail.com>
2025-02-28 11:28:49 +0100

    fix tolerance slider

--- app/assess-progress/index.tsx
+++ app/assess-progress/index.tsx
@@ -1,6 +1,6 @@
 import { PageView, Loader, Header, SliderInput, Spacer } from '@/components/ui';
-import { useQuery } from '@/lib/client';
 import { AssessProgressDocument } from '@/graphql';
+import { useQuery } from '@/lib/client';
 
 export default function AssessProgress() {
 	const [data, error, loading, retry] = useQuery<AssessProgressQuery>(AssessProgressDocument);
@@ -11,7 +11,7 @@ export default function AssessProgress() {
 
 	return (
 		<PageView>
-			<Spacer size='small' />
+			<Spacer size='medium' />
 			<Header size='medium'>Skatta framsteg sedan senaste samtalet</Header>
 			{sofAssessProgress?.inputs.map(({ id, label, slug, min, max }) => (
 				<SliderInput key={id} id={id} label={label} slug={slug} min={min} max={max} />
--- app/tolerance-window/index.tsx
+++ app/tolerance-window/index.tsx
@@ -1,6 +1,6 @@
 import { Loader, TextInput, PageView, Spacer, Text } from '@/components/ui';
-import VerticalSlider from 'rn-vertical-slider';
-import { StyleSheet, View } from 'react-native';
+import VerticalSlider from '@/components/VerticalSlider';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
 import { useQuery } from '@/lib/client';
 import { ToleranceWindowDocument } from '@/graphql';
 import StructuredContent from '@/components/StructuredContent';
@@ -9,7 +9,12 @@ import Theme from '@/styles/theme';
 
 const sliderWidth = 30;
 const sliderHeight = Theme.screenHeight / 2;
-const sliderThumbWidth = 50;
+const sliderThumbWidth = 30;
+const levels = [
+	'Över - stark rädsla, raseri',
+	'Mitt i - i balans, kan hantera och vara fokuserad',
+	'Under - avsängd, bortdomnad, frånvarande, känns ovärkligt, överanpassar mig',
+];
 
 export default function ToleranceWindows() {
 	const [data, error, loading, retry] = useQuery<ToleranceWindowQuery>(ToleranceWindowDocument);
@@ -33,62 +38,55 @@ export default function ToleranceWindows() {
 }
 
 function ToleranceVerticalSlider() {
-	const [value, setValue] = React.useState(0);
+	const [value, setValue] = React.useState(1);
 
 	return (
 		<View style={s.view}>
 			<View style={s.slider}>
 				<VerticalSlider
 					value={value}
-					onChange={(value) => setValue(value)}
+					onChange={(value: number) => setValue(value)}
 					height={sliderHeight}
 					width={sliderWidth}
+					renderIndicatorHeight={sliderThumbWidth}
 					step={1}
-					min={1}
-					max={3}
+					min={0}
+					max={levels.length - 1}
 					borderRadius={0}
 					minimumTrackTintColor='transparent'
 					maximumTrackTintColor='transparent'
 					showIndicator={true}
-					animationConfig={{}}
+					animationConfig={{ damping: 30, stiffness: 500 }}
 					renderIndicator={() => (
 						<View
 							style={{
-								height: sliderWidth,
-								width: sliderWidth,
-								justifyContent: 'center',
-								alignItems: 'center',
+								backgroundColor: Theme.color.black,
+								width: sliderThumbWidth,
+								height: sliderThumbWidth,
 							}}
-						>
-							<View
-								style={{
-									backgroundColor: Theme.color.black,
-									width: sliderThumbWidth,
-									height: sliderThumbWidth,
-									borderRadius: sliderThumbWidth / 2,
-								}}
-							></View>
-						</View>
+						></View>
 					)}
-					containerStyle={{ borderRadius: 0, zIndex: 100 }}
-					sliderStyle={{ borderRadius: 5 }}
+					containerStyle={{
+						borderRadius: 0,
+						zIndex: 100,
+						height: sliderHeight,
+						width: sliderWidth,
+					}}
+					sliderStyle={{ borderRadius: 0 }}
 				/>
+				<View style={s.temp}>
+					<View style={[s.tempSection, { backgroundColor: 'red' }]} />
+					<View style={[s.tempSection, { backgroundColor: 'green' }]} />
+					<View style={[s.tempSection, { backgroundColor: 'gray' }]} />
+				</View>
 			</View>
+
 			<View style={s.level}>
-				<View style={s.step}>
-					<Text>Över - stark rädsla, raseri</Text>
-				</View>
-				<View style={s.step}>
-					<Text>Mitt i - i balans, kan hantera och vara fokuserad</Text>
-				</View>
-				<View style={s.step}>
-					<Text>Under - avsängd, bortdomnad, frånvarande, känns ovärkligt, överanpassar mig</Text>
-				</View>
-			</View>
-			<View style={s.temp}>
-				<View style={[s.tempSection, { backgroundColor: 'red' }]} />
-				<View style={[s.tempSection, { backgroundColor: 'green' }]} />
-				<View style={[s.tempSection, { backgroundColor: 'gray' }]} />
+				{levels.map((text, idx) => (
+					<View style={s.step}>
+						<Text style={value === levels.length - 1 - idx && s.selected}>{text}</Text>
+					</View>
+				))}
 			</View>
 		</View>
 	);
@@ -98,14 +96,27 @@ const s = StyleSheet.create({
 	view: {
 		flex: 1,
 		flexDirection: 'row',
-		height: sliderHeight,
 		width: Theme.screenWidth - Theme.margin * 2,
-		marginBottom: Theme.margin,
 	},
 	slider: {
+		flex: 0,
 		width: sliderWidth,
 		height: sliderHeight,
 		flexBasis: sliderWidth,
+	},
+	temp: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		zIndex: 0,
+		width: sliderWidth,
+		height: sliderHeight,
+		flex: 1,
+		flexDirection: 'column',
+		backgroundColor: Theme.color.green,
+	},
+	tempSection: {
+		flexBasis: '33.3333%',
 	},
 	level: {
 		flexDirection: 'column',
@@ -118,18 +129,7 @@ const s = StyleSheet.create({
 		justifyContent: 'center',
 		flex: 1,
 	},
-	temp: {
-		position: 'absolute',
-		top: 0,
-		left: 0,
-		zIndex: 0,
-		width: sliderWidth,
-		height: sliderHeight,
-		overflow: 'visible',
-		flexDirection: 'column',
-		backgroundColor: Theme.color.green,
-	},
-	tempSection: {
-		flexBasis: '33.3333%',
+	selected: {
+		fontWeight: 'bold',
 	},
 });
