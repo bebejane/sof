3a88a6cc860d4e613c98ecf6f7095ccf1de4f5f2
Bebe Jane <bjornthief@gmail.com>
2025-03-14 10:04:25 +0100

    fix

--- app/create-everyday-flow/index.tsx
+++ app/create-everyday-flow/index.tsx
@@ -1,4 +1,5 @@
-import { Paragraph, PageView, Loader, TextInput, ReadMoreContent } from '@/components/ui';
+import { Paragraph, PageView, Loader, TextInput } from '@/components/ui';
+import ReadMoreContent from '@/components/ReadMoreContent';
 import { useQuery } from '@/lib/client';
 import { CreateEverydayFlowDocument } from '@/graphql';
 import StructuredContent from '@/components/StructuredContent';
